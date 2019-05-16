801b9b1fa6a2a475ade6348b20ec6bd6f33b5a3f
mrbenino <beninbenino@gmail.com>
2019-05-16 15:14:34 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php 
+  $dbServername = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbName = "map";
+
+  $conn = mysqli_connect( $dbServername, $dbUsername, $dbPassword, $dbName);
+  $result = mysqli_query($conn, "SELECT * FROM markers");
+  $resultCheck = mysqli_num_rows($result);
+
+  if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      print_r($row);
+    }
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +23,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
       #map {
         height: 100%;
       }
-      /* Optional: Makes the sample page fill the window. */
       html, body {
         height: 100%;
         margin: 0;
@@ -21,9 +35,13 @@
 </head>
 <body>
     <h1>oops!</h1>
+
     <div id="map"></div>
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="index.js"></script>
+
+    <script src="template/js/gmamm.js"></script>
+      
     <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJtiblvFBLLTpcamTLnXBQ7udOMNlrieQ&callback=initMap"></script>
 </body>
